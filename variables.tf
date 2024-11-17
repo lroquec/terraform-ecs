400465fa00b62f94826c49f186a185ee8818d95b
Laura Roque <lroquecu@gmail.com>
2024-11-17 10:10:08 +0100

    add variables for networking

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+
+  validation {
+    condition     = can(cidrnetmask(var.vpc_cidr))
+    error_message = "The VPC CIDR block is not a valid CIDR block"
+  }
+}
+
+variable "subnet_config" {
+  type = map(object({
+    cidr_block = string
+    public     = optional(bool, false)
+  }))
+
+  default = {
+    subnet1 = {
+      cidr_block = "10.0.1.0/24"
+      public     = true
+    }
+    subnet2 = {
+      cidr_block = "10.0.2.0/24"
+      public     = false
+    }
+  }
+
+  validation {
+    condition = alltrue([
+      for config in values(var.subnet_config) : can(cidrnetmask(config.cidr_block))
+    ])
+    error_message = "The cidr_block config option must contain a valid CIDR block."
+  }
+}
