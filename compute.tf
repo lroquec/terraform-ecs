a441266f9fbc7a5eb739b20c9e54499e8162d7df
Laura Roque <lroquecu@gmail.com>
2024-11-17 10:32:46 +0100

    add alb listener

--- compute.tf
+++ compute.tf
@@ -81,3 +81,14 @@ tags = merge(local.common_tags, {
     Name = "${local.project_name}-tg"
   })  
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
