2ae6dd9497d50b7c4f2ca7ec308ea43fd03f083f
Laura Roque <lroquecu@gmail.com>
2024-11-17 10:19:30 +0100

    add security group for ALB

---
+++ compute.tf
@@ -0,0 +1,23 @@
+# Security Groups
+resource "aws_security_group" "alb" {
+  description = "Security group for ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-alb-sg"
+  })
+}
