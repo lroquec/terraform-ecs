a59899f36f65d51f7d00de358dacd0532758fcdf
Laura Roque <lroquecu@gmail.com>
2024-11-17 10:26:35 +0100

    sg for task

--- compute.tf
+++ compute.tf
@@ -24,3 +24,27 @@ resource "aws_security_group_rule" "allow_http_inbound" {
   security_group_id = aws_security_group.allow_http_inbound.id
 }
 
+resource "aws_security_group" "allow_container_inbound" {
+  vpc_id = module.vpc.vpc_id
+  tags = merge(local.common_tags, {
+    Name = "${local.project_name}-allow-http-sg"
+  })
+}
+
+resource "aws_security_group_rule" "allow_container_all_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_container_inbound.id
+}
+
+resource "aws_security_group_rule" "allow_container_inbound" {
+  type              = "ingress"
+  from_port         = 5000
+  to_port           = 5000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_container_inbound.id
+}
