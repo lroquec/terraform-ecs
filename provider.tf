d541ffa3f2e408903ada74b3215e7d50bba027fb
Laura Roque <lroquecu@gmail.com>
2024-11-17 10:08:30 +0100

    add aws provider and set up versions

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.76.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      CreatedBy = "lroquec"
+      Owner     = "DevOps Team"
+    }
+  }
+}
