51080c80d6482b83e8930a69a25e85b1e1973815
Laura Roque <lroquecu@gmail.com>
2024-11-17 10:11:53 +0100

    add networking resources

---
+++ networking.tf
@@ -0,0 +1,22 @@
+locals {
+  private_subnets_cidr = [for k, v in var.subnet_config : v.cidr_block if !v.public]
+  public_subnets_cidr  = [for k, v in var.subnet_config : v.cidr_block if v.public]
+}
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.3"
+
+  name                    = "${local.project_name}-vpc"
+  cidr                    = var.vpc_cidr
+  azs                     = data.aws_availability_zones.azs.names
+  private_subnets         = local.private_subnets_cidr
+  public_subnets          = local.public_subnets_cidr
+  map_public_ip_on_launch = true
+
+  tags = merge(local.common_tags, {
+    "CostCenter" = "FreeTier"
+  })
+}
