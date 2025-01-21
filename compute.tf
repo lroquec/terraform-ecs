a34bcf44cad9fbd93b9cb5f7f73935a914d38d93
Laura Roque <lroquecu@gmail.com>
2025-01-21 15:26:34 +0100

    feat: parameterize health check command in ECS task definition

--- compute.tf
+++ compute.tf
@@ -186,7 +186,7 @@ resource "aws_ecs_task_definition" "main" {
         }
       }
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:5000/ || exit 1"]
+        command     = ["CMD-SHELL", "curl -f http://localhost:${var.container_port}/ || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
